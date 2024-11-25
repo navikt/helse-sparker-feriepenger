6bf70edf5c344359f16393583b4bb17de4e857be
david steinsland <david@davidsteinsland.net>
2024-11-25 10:29:12 +0100

    bumper tbd-libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ val logbackClassicVersion = "1.5.12"
 val logbackEncoderVersion = "8.0"
 val hikariCPVersion = "6.1.0"
 val postgresqlVersion = "42.7.4"
-val tbdLibsVersion = "2024.11.24-12.01-42fdc22d"
+val tbdLibsVersion = "2024.11.25-10.23-348b187b"
 val cloudSqlVersion = "1.21.0"
 
 plugins {
